39a7619c00fbea9e89ce17a7c26b1310bd170ccf
marccollin <marccollin@hotmail.fr>
2017-04-27 12:51:39 +0200

    first commit

--- src/test/java/TestConnexionAdminOK.java
+++ src/test/java/TestConnexionAdminOK.java
@@ -5,6 +5,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
